7d9e3bb84e2deda891689ffa3faa716dc4abc3f8
carefree <946715806@qq.com>
2019-12-24 17:20:05 +0800

    update

--- KFBlankSlate/KFBlankSlateGeneralHandler.m
+++ KFBlankSlate/KFBlankSlateGeneralHandler.m
@@ -372,17 +372,6 @@
     }
 }
 
-- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
-    // 判断otherGestureRecognizer是不是系统pop手势
-    if ([otherGestureRecognizer.view isKindOfClass:NSClassFromString(@"UILayoutContainerView")]) {
-        // 判断系统手势的state是began还是fail，同时判断scrollView的位置是不是正好在最左边
-        if (otherGestureRecognizer.state == UIGestureRecognizerStateBegan && self.scrollView.contentOffset.x == 0) {
-            return YES;
-        }
-    }
-    return NO;
-}
-
 #pragma mark - Private
 - (void)setObject:(id)object inDictionary:(NSMutableDictionary *)dictionary forState:(KFDataLoadState)state {
     if (state & KFDataLoadStateIdle) {
